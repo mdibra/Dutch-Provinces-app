6db9596507f06b9a5ed585cf3b8ea98e24849c11
Mohammad <md_ibra@hotmail.com>
2017-03-02 21:28:18 +0100

    Update service.js

--- js/services/service.js
+++ js/services/service.js
@@ -1,5 +1,6 @@
 function getprovincies($http) {
-	var url = 'http://api-nghyf.rhcloud.com/provinces';
+	// var url = 'http://api-nghyf.rhcloud.com/provinces';
+	var url = 'js/data.json';
 	getprovincies = this;
 	getprovincies.bringData = function () {
 		
